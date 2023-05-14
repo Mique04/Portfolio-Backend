dcc4638b8740b5d2a06322dd9f3175e921b9ed4a
Miqueas <miqueassantillan2704@gmail.com>
2023-05-14 15:34:27 -0300

    detalles finales

--- src/main/java/com/miqueassantillan/Portfolio/Web/controller/Controller.java
+++ src/main/java/com/miqueassantillan/Portfolio/Web/controller/Controller.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class Controller {
 
     @Autowired
@@ -74,4 +74,6 @@ public class Controller {
         }
         return ResponseEntity.ok("Usuario editado exitosamente");
     }
+    
+    
 }
