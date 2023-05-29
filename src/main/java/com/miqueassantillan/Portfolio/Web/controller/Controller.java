8871e5122b523c1f55e2c269c6bfa7c6d0d524a2
Miqueas <miqueassantillan2704@gmail.com>
2023-05-29 20:37:34 -0300

    terminado2

--- src/main/java/com/miqueassantillan/Portfolio/Web/controller/Controller.java
+++ src/main/java/com/miqueassantillan/Portfolio/Web/controller/Controller.java
@@ -7,7 +7,7 @@ import com.miqueassantillan.Portfolio.Web.validation.ValidacionUsuario;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@CrossOrigin(origins = "https://portfolio-mique2704.web.app")
+@CrossOrigin(origins = "https://portfolio-mique2704.web.app")
 public class Controller {
 
     @Autowired
